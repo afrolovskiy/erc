c58b4a9e021992066a175636d7f941662bbc24c8
Alexey <afrolovskiy@localhost.localdomain>
2014-08-10 20:54:10 +0400

    init main page template

--- erc/urls.py
+++ erc/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from erc import views
+
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'erc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 )
